cc63f25154411c35003b480839b90406da89ec7d
Antônio Vinícius Menezes Medeiros <vinyanalista@gmail.com>
2017-04-10 22:23:07 -0300

    Patterns (base, x11 and kde) and packages (sddm) of a clean openSUSE Leap 42.2 install with KDE Desktop (as found in http://download.opensuse.org/distribution/leap/42.2/repo/oss/control.xml)

--- create_appliance.sh
+++ create_appliance.sh
@@ -9,7 +9,7 @@
 # Contact: feedback@susestudio.com
 # ============================================================================
 
-image_file='image/Linux_Kamarada_42.2_KDE.x86_64-0.0.1.raw'
+image_file='image/Linux_Kamarada_42.2_KDE.x86_64-0.0.3.raw'
 image_arch='x86_64'
 schema_ver='5.2'
 base_system='Leap_42.1'
